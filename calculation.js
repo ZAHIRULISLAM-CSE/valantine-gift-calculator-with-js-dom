a4ce81c56fbe97a3858e2ad6715cde30492887db
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-02-14 01:54:31 +0600

    simple gift calculator with javascript

---
+++ calculation.js
@@ -0,0 +1,86 @@
+document.getElementById("kitkat-buy-btn").addEventListener("click",function(){
+   const kitkatQuantity= getFieldQuantityValue("kitkat-quantity");
+   if(kitkatQuantity<0){
+    alert("provide positive input");
+    document.getElementById("kitkat-quantity").value="";
+    return;
+   }
+   if(isNaN(kitkatQuantity)){
+    alert("provide valid input");
+    document.getElementById("kitkat-quantity").value="";
+    return;
+   }
+   const kitkatPrice = totalPrice(kitkatQuantity,200);
+   setPrice("chocolate",kitkatPrice);
+   setTotalPrice(kitkatPrice);
+   document.getElementById("kitkat-quantity").value="";
+})
+document.getElementById("rose-buy-btn").addEventListener("click",function(){
+    const roseQuantity= getFieldQuantityValue("rose-quantity");
+    if(roseQuantity<0){
+        alert("provide positive input");
+        document.getElementById("rose-quantity").value="";
+        return;
+       }
+       if(isNaN(roseQuantity)){
+        alert("provide valid input");
+        document.getElementById("rose-quantity").value="";
+        return;
+       }
+    const rosePrice = totalPrice(roseQuantity,100);
+    setPrice("rose",rosePrice);
+    setTotalPrice(rosePrice);
+    document.getElementById("rose-quantity").value="";
+ })
+ document.getElementById("diary-buy-btn").addEventListener("click",function(){
+    const diaryQuantity= getFieldQuantityValue("diary-quantity");
+    if(diaryQuantity<0){
+        alert("provide positive input");
+        document.getElementById("diary-quantity").value="";
+        return;
+       }
+       if(isNaN(diaryQuantity)){
+        alert("provide valid input");
+        document.getElementById("diary-quantity").value="";
+        return;
+       }
+    const diaryPrice = totalPrice(diaryQuantity,100);
+    setPrice("diary",diaryPrice);
+    setTotalPrice(diaryPrice);
+    document.getElementById("diary-quantity").value="";
+ })
+ let count=0;
+ document.getElementById("btn-apply").addEventListener("click",function(){
+    if(count ==1){
+        return;
+    }
+    const promoCode=document.getElementById("promo-code").value;
+    currentTotal= document.getElementById("all-total").innerText;
+    if(promoCode == 101){
+        discount=currentTotal*0.1;
+        count++;
+    }
+    document.getElementById("all-total").innerText=currentTotal-discount;
+
+ })
+function getFieldQuantityValue(id){
+   let quantity= document.getElementById(id).value;
+   quantity=parseInt(quantity);
+   return quantity;
+}
+function totalPrice(quantity,price){
+    return quantity*price;
+}
+function setPrice(id,itemPrice){
+    let previousValue=document.getElementById(id).innerText;
+    previousValue=parseFloat(previousValue);
+    const newPrice=previousValue+itemPrice;
+    document.getElementById(id).innerText=newPrice;
+}
+function setTotalPrice(singleItemPrice){
+    let previousTotal=document.getElementById("total").innerText;
+    previousTotal=parseFloat(previousTotal);
+    const currentTotal=previousTotal+singleItemPrice;
+    document.getElementById("total").innerText=currentTotal;
+    document.getElementById("all-total").innerText=currentTotal;
+}
